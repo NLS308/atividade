cd86e0c6fca4c355bfc9b99c56a518dacb6224c3
NLS308 <saquetto.nikolas@escola.pr.gov.br>
2025-03-26 10:48:20 -0300

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    
+int main(){
+    float real, dolar;
+    scanf ("%f", &dolar);
+    real = (dolar * 5.9693);
+    printf ("real = %.2f, dolar = %.2f", real, dolar );
+    return 0;
+    
+}
